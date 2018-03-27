77f1d77a54275bb2af77a47e4f6822efc5a86e4e
Tim Sawyer <tim.git@brassbandresults.co.uk>
2018-03-27 21:12:25 +0100

    Fix some unicode issues Progress with getting addresults to work

--- bbr3/siteutils.py
+++ bbr3/siteutils.py
@@ -5,39 +5,14 @@ import http.client
 import re
 import unicodedata
 from django.conf import settings
+from django.utils.encoding import smart_text
+from django.utils.text import slugify as django_slugify
 
 
 def slugify(s, entities=True, decimal=True, hexadecimal=True, instance=None, slug_field='slug', filter_dict=None):
-    s = smart_unicode(s)
-
-    #character entity reference
-    if entities:
-        s = re.sub('&(%s);' % '|'.join(name2codepoint), lambda m: unichr(name2codepoint[m.group(1)]), s)
-
-    #decimal character reference
-    if decimal:
-        try:
-            s = re.sub('&#(\d+);', lambda m: unichr(int(m.group(1))), s)
-        except:
-            pass
-
-    #hexadecimal character reference
-    if hexadecimal:
-        try:
-            s = re.sub('&#x([\da-fA-F]+);', lambda m: unichr(int(m.group(1), 16)), s)
-        except:
-            pass
-
-    #translate
-    s = unicodedata.normalize('NFKD', s).encode('ascii', 'ignore')
-
-    #replace unwanted characters
-    s = re.sub(r'[^-a-z0-9]+', '-', s.lower())
-
-    #remove redundant -
-    s = re.sub('-{2,}', '-', s).strip('-')
-
+    s = django_slugify(s)
     slug = s
+
     if instance:
         def get_query():
             query = instance.__class__.objects.filter(**{slug_field: slug})
